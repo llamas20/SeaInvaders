740e64c0826c68d42886a3b0680a15f857553dd8
llamas20 <jerryvogel3@gmail.com>
2018-12-02 21:12:06 -0500

    Update to tests
    I updated the tests.  :)

--- exampletest.h
+++ exampletest.h
@@ -1,5 +1,6 @@
 #include <cxxtest/TestSuite.h>
 #include <Model.h>
+#include <vector>
 
 class ExampleSuite : public CxxTest::TestSuite {
  public:
@@ -7,18 +8,26 @@ class ExampleSuite : public CxxTest::TestSuite {
     Test methods on Invaders.
    */
   void testIsHit() {
-    // TODO
+    Invader inv(150, 5, 2);
+    Starfish starf(150, 0, true);
+    Starfish stafy(100, 0, true);
+    TS_ASSERT_EQUALS(inv.isHit(starf), true);
+    TS_ASSERT_EQUALS(inv.isHit(stafy), false);
   }
+  
   void testIsDefeated() {
-    // TODO
+    Invader inv(150, 5, 2);
+    Invader dedded(100, 0, 0);
+    TS_ASSERT_EQUALS(inv.isDefeated(), false);
+    TS_ASSERT_EQUALS(dedded.isDefeated(), true);
   }
   void testMoveInvader() {
-    Invader inv{150, 5, 2};
+    Invader inv(150, 5, 2);
     inv.moveInvader();
     TS_ASSERT_EQUALS(inv.getY(), -5);
   }
   void testGetXY() {
-    Invader inv{140, 10, 1};
+    Invader inv(140, 10, 1);
     TS_ASSERT_EQUALS(inv.getX(), 140);
     TS_ASSERT_EQUALS(inv.getY(), -10);
   }
@@ -34,18 +43,38 @@ class ExampleSuite : public CxxTest::TestSuite {
     TS_ASSERT_EQUALS(model.getPlayerX(), 190);
   }
   void testShoot() {
-    // TODO
+    UnderSeaInvadersModel model;
+    TS_ASSERT_EQUALS(model.star.shot == false);
+    model.shoot();
+    TS_ASSERT_EQUALS(model.star.shot == true);
   }
   void testMoveStarfish() {
-    // TODO
-  }
-  void testMoveInvaders() {
-    // TODO
+    UnderSeaInvadersModel model;
+    TS_ASSERT_EQUALS(model.star.y, 200);
+    model.moveStarfish();
+    TS_ASSERT_EQUALS(model.star.y, 195);
   }
   void testGenerateInvaders() {
-    // TODO
+    Invader inv(150, 5, 2);
+    std::vector<Invader> invs{inv};
+    UnderSeaInvadersModel model;
+    model.generateInvaders(invs);
+    TS_ASSERT_EQUALS(model.invaders, invs);
+  }
+  void testMoveInvaders() {
+    Invader inv(150, 5, 2);
+    std::vector<Invader> invs{inv};
+    UnderSeaInvadersModel model;
+    model.generateInvaders(invs);
+    model.moveInvaders();
+    TS_ASSERT_EQUALS(model.invaders[0].getY(), 5);
   }
   void testRemoveInvaders() {
-    // TODO
+    Invader inv(150, 5, 0);
+    std::vector<Invader> invs{inv};
+    UnderSeaInvadersModel model;
+    model.generateInvaders(invs);
+    model.removeInvaders();
+    TS_ASSERT_EQUALS(model.invaders, {});
   }
 };
